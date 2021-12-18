881181671016adc89110b6a3bde7d949b2cc2e53
Velina Dimova <79513153+VellyD@users.noreply.github.com>
2021-12-18 12:54:01 +0200

    removing unused parameter

--- sustainability-blog/src/services/authService.js
+++ sustainability-blog/src/services/authService.js
@@ -1,12 +1,12 @@
 const baseUrl = "http://localhost:3030";
 
-export const register = ({ email, password, nikname }) => {
+export const register = ({ email, password }) => {
   return fetch(`${baseUrl}/users/register`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
     },
-    body: JSON.stringify({ email, password, nikname }),
+    body: JSON.stringify({ email, password }),
   }).then((res) => res.json());
 };
 
