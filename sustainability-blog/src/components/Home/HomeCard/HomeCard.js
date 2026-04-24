8e1d872bd58097b7f9c166183f6e2da41c23a91c
VellyD <viddvd3@gmail.com>
2026-04-24 12:23:34 +0300

    checking the css of the product card.

--- sustainability-blog/src/components/Home/HomeCard/HomeCard.js
+++ sustainability-blog/src/components/Home/HomeCard/HomeCard.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./homeCard.css";
+import "../../Products/ProductCard.js/productCard.css";
 
 export const HomeCard = ({ productCard }) => {
   return (
